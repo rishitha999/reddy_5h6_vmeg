e714acf1ca9c8f03b402237dde2f02dd6ba52622
rishitha999 <rishitha.uppunuri@gmail.com>
2017-09-08 16:54:38 +0530

    Update Main.java

--- src/src/Main.java
+++ src/src/Main.java
@@ -5,6 +5,32 @@ public class Main {
 		//You may test that your code works find here
 		//Please check that your code works and has no 
 		//compilation problems before to submit
+		StudentGroup s=new StudentGroup();
+		student[] s1=s.getStudents();
+		s.setStudents(s1);
+		Student stu=s.getStudent(index);
+		s.setStudent(student,index);
+		s.add(student,index);
+		s.addFirst(student);
+		s.addLast(student);
+		s.remove(index);
+		s.remove(student);
+		s.removeFromIndex(index);
+		s.removeFromElement(student);
+		s.removeToIndex(index);
+		s.removeToElement(student);
+		s.bubbleSort();
+		s.getByBirthDate(date);
+		s.getBetweenBirthDates( firstDate,lastDate);
+		s.getNearBirthDate(date, days);
+		s.getCurrentAgeByDate( indexOfStudent);
+		s.getStudentsByAge(age);
+		s.getStudentsWithMaxAvgMark();
+		s.getNextStudent(student);
+		
+	
+		
+		
 	}
 
 }
